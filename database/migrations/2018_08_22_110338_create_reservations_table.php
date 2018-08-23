39ea6e21d39eec3f0bbd1c9bdd73425d33fe91fb
chathura002 <chathura002@gmail.com>
2018-08-23 13:06:36 +1200

    Modify data with Eloquent

---
+++ database/migrations/2018_08_22_110338_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date_in');
+            $table->date('date_out');
+            $table->integer('client_id')->unasinged();
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->integer('room_id')->unasinged();
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reservations');
+    }
+}
