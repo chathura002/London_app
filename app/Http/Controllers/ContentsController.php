50c9a8d557425bedece93d11cdb5d450931cd7f6
chathura002 <chathura002@gmail.com>
2018-09-03 12:09:07 +1200

    CRRF protection and session

--- app/Http/Controllers/ContentsController.php
+++ app/Http/Controllers/ContentsController.php
@@ -9,9 +9,11 @@ use App\Http\Requests;
 class ContentsController extends Controller
 {
     //
-    public function home(){
+    public function home(Request $request){
         $data = [];
         $data['version'] = '1.0.2';
+        $last_updated = $request->session()->has('last_updated') ? $request->session()->pull('last_updated') :'none';
+        $data['last_updated'] = $last_updated;
         return view('contents/home',$data);
     }
 }
